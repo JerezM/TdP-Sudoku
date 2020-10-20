537926986589f32a1d1d1f8f3e93851fd49dd143
JerezM <jerez149@gmail.com>
2020-10-20 19:53:09 -0300

    Sudoku main class added

---
+++ src/main/Sudoku.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.EventQueue;
+
+import main.view.GUI;
+
+public class Sudoku {
+        
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					GUI frame = GUI.getInstance();
+                    frame.setVisible(true);
+                    
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+    }
+}
